531e89621b59c20422d3a0773de3559a00b625a5
Martin Moldsvor <martin@moldsvor.com>
2019-11-04 12:51:21 +0100

    Updated example to show how to get position

--- example.py
+++ example.py
@@ -4,7 +4,6 @@ node_left = Node('COM2', Vector2D(0, 0))
 node_right = Node('COM2', Vector2D(100, 0))
 processing_unit = ProcessingUnit([node_left, node_right], (0, 0, 100, 100))
 
-# Get the position
 while True:
     position = processing_unit.position
     if position is not None:
