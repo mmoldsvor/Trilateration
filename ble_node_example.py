68469c560c55bf053534fd86881978c859b20b0d
Martin Moldsvor <martin@moldsvor.com>
2019-11-04 21:54:10 +0100

    Option to input environmental constant to rssi_to_instance function

--- ble_node_example.py
+++ ble_node_example.py
@@ -2,13 +2,14 @@ from trilateration import Node
 
 
 class BLENode(Node):
-    def __init__(self, position, connection, measured_power=-50, default_distance=100):
+    def __init__(self, position, connection, measured_power=-50, environmental_constant=3, default_distance=100):
         """
         A Node which supports BLE and rssi to distance conversion
         :param measured_power: The RSSI measured at 1 meter
         """
         super().__init__(position, connection, default_distance)
         self.measured_power = measured_power
+        self.environmental_constant = environmental_constant
 
     def filter_data(self):
         """
@@ -27,15 +28,15 @@ class BLENode(Node):
         """
         if self.connection.connected:
             filtered_data = self.filter_data()
-            self.distance = self.rssi_to_distance(filtered_data, 2)
+            self.distance = self.rssi_to_distance(filtered_data)
         else:
             self.distance = self.default_distance
 
-    def rssi_to_distance(self, rssi, constant=3):
+    def rssi_to_distance(self, rssi):
         """
         Converts from rssi to distance
         :param rssi: int, current RSSI
         :param constant: int, Environmental factor normally in the range of [2, 4]
         :return: int, distance measured in meters
         """
-        return 10 ** ((self.measured_power - rssi) / (10 * constant))
+        return 10 ** ((self.measured_power - rssi) / (10 * self.environmental_constant))
