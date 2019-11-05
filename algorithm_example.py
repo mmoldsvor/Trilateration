27949a59d06386eb0662a81f3b38840b395317d2
Martin Moldsvor <martin@moldsvor.com>
2019-11-05 08:12:41 +0100

    Added custom positioning algorithm example

---
+++ algorithm_example.py
@@ -0,0 +1,21 @@
+from trilateration import Node, ProcessingUnit, Vector2D, SerialConnection
+
+
+def pick_first(intersections):
+    """
+    DO NOT USE FOR POSITIONING
+    Just a basic algorithm that picks the first intersection it can find
+    :return: Vector()
+    """
+    return intersections[0]
+
+
+node_left = Node(Vector2D(0, 0), SerialConnection('COM9', 38400, 20))
+node_right = Node(Vector2D(100, 0), SerialConnection('COM13', 38400, 20))
+processing_unit = ProcessingUnit([node_left, node_right], (0, 0, 100, 100), algorithm_function=pick_first)
+
+while True:
+    position = processing_unit.position
+    print(node_left.distance, node_right.distance)
+    if position is not None:
+        print(position)
--- visualizer_example.py
+++ visualizer_example.py
@@ -3,7 +3,8 @@ from visualizer import AnalysisTool
 
 node_left = Node(Vector2D(100, 100), SerialConnection('COM9', 38400, 20))
 node_right = Node(Vector2D(400, 100), SerialConnection('COM13', 38400, 20))
-processing_unit = ProcessingUnit([node_left, node_right], (100, 100, 400, 400))
+node_bottom = Node(Vector2D(250, 400), SerialConnection('COM15', 38400, 20))
+processing_unit = ProcessingUnit([node_left, node_right, node_bottom], (100, 100, 400, 400))
 
 while True:
     analysis = AnalysisTool()
